a6a78508a98d2d084252f93af5588fd50782830e
krymtkts <takatoshi.kuriyama@gmail.com>
2024-12-28 17:14:28 +0900

    Update for the testing.

--- UnitTest1.fs
+++ UnitTest1.fs
@@ -1,7 +1,10 @@
 module UnitTest1
 
 open Xunit
+open FsUnit.Xunit
 
 [<Fact>]
-let Test1() =
-    Assert.True(true)
+let Test1 () = Assert.True(true)
+
+[<Fact>]
+let Test2 () = true |> should equal true
